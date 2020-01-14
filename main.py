d6b327b9204d356f4bc5536ba861d4a61eca5531
arthurbatista04 <arthurbatista04@outlook.com>
2020-01-14 11:21:08 -0300

    finished avaliation and selection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from copy import copy
-from random import
+from copy import deepcopy
+import random
+from math import inf
+import sys
+n = 100  # number of population
 
 
 class Vertice(object):
@@ -8,6 +11,14 @@ class Vertice(object):
         self.y = y
         self.cap = cap - demand
         self.demand = demand
+        self.id = random.random()
+
+
+class Individual(object):
+    def __init__(medians, fitness):
+        self.medians = medians
+        self.fitness = fitness
+        self.newIndivual = True
 
 
 def inputParse(input):
@@ -20,20 +31,71 @@ def inputParse(input):
     return [lines, vertices, medians]
 
 
-def inicialSol(inicialSet):
-    inicialSol = []
-    numbers = random.randint(0, vertices)
-    for i in medians:
-        inicialSol.append(inicialSet.pop(numbers[i]))
+def generateRandomSolution(genes, numVertices, numMedians):
+    # Generates a random solution of medianas vertices
+    count = 0
+    solution = {}
+    for i in range(numMedians):
+        count += 1
+        rand = random.randint(0, numVertices-count)
+        candidate = genes.pop(rand)
+        solution[candidate.id] = candidate
+    return [solution, genes]
 
 
-def calculateEdges(newMedians, inicialSet):
-    # calcular as distancias para cada vertice com cada mediana e escolher a menor
+def generateRandomPopulation(n, genes, numVerties, numMedians):
+    return [generateRandomSolution(deepcopy(genes), int(numVerties), int(numMedians)) for i in range(n)]
+
+
+def distance(x, x0, y, y0): return ((x-x0)**2 + (y-y0)**2)**0.5
+
+
+def neighborhoodDistance(solution, population):
+    # find the neartes neighbors to the medians and calculate there distance
+    distances = {median: 0 for median in solution}
+    for individual in population:
+        minNodes = {'value': inf}
+        for medianId in solution:
+            dis = distance(individual.x, solution[medianId].x,
+                           individual.y, solution[medianId].y)
+            if dis < minNodes['value'] and solution[medianId].cap >= individual.demand:
+                minNodes['value'] = dis
+                minNodes[individual.id] = medianId
+        # found new edge, substract its demand from median capacity
+
+        medianId = minNodes[individual.id]
+        median = solution[medianId]
+        median.cap -= individual.demand
+        # calculate the distance to the median
+        distances[medianId] += distance(solution[medianId].x,
+                                        individual.x, solution[medianId].y, individual.y)
+    return distances
+
+
+def fitness(solution, genes):
+    distances = neighborhoodDistance(solution, genes)
+    fit = 0
+    for medianId in distances:
+        fit += distances[medianId]
+        return fit
+
+def inicialFitness(population):
+    for solution,genes in population:
+        fit = fitness(solution,genes)
+        newSolution = Individual(solution,fit)
+    #return heap 
+
+def tournament(population):
+    number = int(n/10)
+
+    for i in number:
+        indiviual = random.uniform(0, n)
 
 
 with open('input.txt') as file:
     lines = file.readlines()
 
-cromos, vertices, medians = inputParse(lines)
-inicialSet = cromos.copy()
-newMedians = inicialSol(inicialSet)
+
+baseGenes, vertices, medians = inputParse(lines)
+population = generateRandomPopulation(n, baseGenes, vertices, medians)
+populationHeap = fitness(population)
